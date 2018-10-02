47aa539b513b712e0ebd5b31a96dd20a23913c98
Jim Aman <jim.a.aman@gmail.com>
2018-10-02 03:33:16 -0500

    added some error handling into main route

--- kycEndpoint/kycapi.py
+++ kycEndpoint/kycapi.py
@@ -1,6 +1,8 @@
 # standard modules
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import traceback
+import datetime
 
 # selfmade modules for defined tasks
 import verifyJWT
@@ -11,22 +13,36 @@ import sendTransaction_Local as sendTx
 # standard instantiantion of the api application through flask
 app = Flask(__name__)
 
+# Define function vars
+errFilePath = lambda ts: './Logs/' + ts + '_errorLog'
+
 # applies the Access-Control-Allow-Origin property to the api route as required by IDM
 CORS(app, resources={r"/kyc": {"origins":"*"}})
 
 # setup the KYC route
 @app.route("/kyc", methods=["POST"])
 def processKYC():
-    payload = verifyJWT.parse_request(request) # verify and retrieve JSON from JWT
-    #recordJSON(payload) # log JSON into text file
-    #tx_hash = sendTx.main(payload['form_data']['btc'])
+    try:
+        payload = verifyJWT.parse_request(request) # verify and retrieve JSON from JWT
+        recordJSON(payload) # log JSON into text file
+        tx_hash = sendTx.main(payload['form_data']['btc']) # send KYC request via Infura API
+
+        return jsonify({"success":True})
+
+    except Exception as e:
+        # Handle exceptions to the process by creating a logfile and 
+        with open(errFilePath(datetime.datetime.now().strftime("%Y.%m.%d.%H%M")),'a+') as errFile:
+            errFile.write(traceback.format_exc())
+
+        return jsonify({"success":False})
+    
   
-    return jsonify({"success":True})
+    
     
 
 @app.route("/test")
 def testFunc():
-    return "Hello World!"
+    return "The server is up and running!"
 
 
 if __name__ == '__main__':
