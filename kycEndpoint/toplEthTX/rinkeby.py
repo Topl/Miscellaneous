6c6204788467343a38326df2495d86b29e1e3d73
Jim Aman <jim.a.aman@gmail.com>
2018-10-10 10:50:06 -0500

    bugs in the rinkeby

--- kycEndpoint/toplEthTX/rinkeby.py
+++ kycEndpoint/toplEthTX/rinkeby.py
@@ -8,9 +8,9 @@ class Rinkeby:
         self.w3 = Web3(Web3.HTTPProvider(settings.API_URL)) # for rinkeby testing
 
         # Set contract addresses as deployed on rinkeby network
-        self.arbits_presale_addr = "0x4393CeF911B451ab098c6973a28C913326E9413E"
-        self.icnq_token_addr = "0x5A22b87A9C0084D49Aa3cb25fD4Ba9aD50323576"
-        self.iconiq_data_pipe_addr = '0xeb828a191f4911d0e371855450ed5d5d057b06b1'
+        self.arbits_presale_addr = '0x4393CeF911B451ab098c6973a28C913326E9413E'
+        self.icnq_token_addr = '0x5A22b87A9C0084D49Aa3cb25fD4Ba9aD50323576'
+        self.iconiq_data_pipe_addr = '0xD86cdd3978D3b293d3e29C6B7eCD76a0360993d8'
 
     def _getKey(self):
         with open(settings.ETH_KEY_PATH) as keyfile: 
@@ -22,14 +22,13 @@ class Rinkeby:
 
     def get_tx_params(self, acct_):
         # Specify tx_parameters
-        return txParams = {
+        return {
             'from': self.w3.toChecksumAddress(acct_),
             'chainId': 4,
             'nonce': self.w3.eth.getTransactionCount(self.w3.toChecksumAddress(acct_)),
             'gas': 100000,
             'gasPrice': self.w3.eth.gasPrice,
             }
-
 
     def add_to_whitelist(self, user_addr):
         # Unlock ethereum account
@@ -39,7 +38,7 @@ class Rinkeby:
         _contract = self.setup_contract_tx(self.arbits_presale_addr, ABI().arbits_presale)
 
         # create various stages of the transaction
-        rawTX = _contract.functions.add_to_whitelist(self.w3.toChecksumAddress(user_addr)).buildTransaction(get_tx_params(toplAcct))
+        rawTX = _contract.functions.add_to_whitelist(self.w3.toChecksumAddress(user_addr)).buildTransaction(self.get_tx_params(toplAcct))
         signTX = toplAcct.signTransaction(rawTX)
 
         # send the final signed transaction
@@ -63,7 +62,7 @@ class Rinkeby:
         _contract = self.setup_contract_tx(self.icnq_token_addr, ABI().icnq_token)
 
         # create various stages of the transaction
-        rawTX =  _contract.functions.set_iconiq_token_amount(self.w3.toChecksumAddress(addr_), 18000000).buildTransaction(get_tx_params(toplAcct))
+        rawTX = _contract.functions.set_iconiq_token_amount(self.w3.toChecksumAddress(addr_), 18000000).buildTransaction(self.get_tx_params(toplAcct))
         signTX = toplAcct.signTransaction(rawTX)
 
         # send the final signed transaction
